a9fc4198982c4312976e09f3d34b397ce6504371
Marion4000 <marion.marrot@gmail.com>
2022-07-21 16:19:49 +0200

    mini correction

--- formulaire.php
+++ formulaire.php
@@ -1,7 +1,7 @@
 <?php
 if( isset($_GET['nom'])&& isset($_GET['id']) ) {
     if( !empty($_GET['nom']&& isset($_GET['id'])) ) {
-        // on mémorise l’nom reçu en GET
+        // on mémorise le nom reçu en GET
         $nom = $_GET['nom'];
         $id = $_GET['id'];
         echo $nom;
